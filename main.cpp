c3bbc3ffef32465254f3e84d03b04bb0c36e8a38
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-03-14 16:36:32 +0200

    Added copyright note

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*************************************************************************
+ Filename appender
+ This little renaming tool was created for Pokemon Infinite Fusion
+ in order to help with renaming alternate sprites in bulk.
+
+    Copyright (C) 2023  Samppa Alatalo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**************************************************************************/
+
 /* Base functionality generated with ChatGPT (https://chat.openai.com) and heavily modified by Samppa Alatalo */
 
 #include <iostream>
